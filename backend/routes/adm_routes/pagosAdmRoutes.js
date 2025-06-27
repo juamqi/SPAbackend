da0d2707aa3e88225ff4ee573d8682f3b871875f
Nicolás Soria <105177329+Nebularmess@users.noreply.github.com>
2025-06-26 22:54:04 -0300

    filtros-ok

--- backend/routes/adm_routes/pagosAdmRoutes.js
+++ backend/routes/adm_routes/pagosAdmRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const pagosAdmController = require('../../controllers/adm_controllers/pagosAdmController');
 
 // GET /api/admin/pagos - Obtiene todos los pagos para la tabla
-router.get('/', pagosAdmController.getAdmPagos);
+router.get('/profesional/:idProfesional', pagosAdmController.getPagosPorProfesional);
+router.get('/servicio/:idServicio', pagosAdmController.getPagosPorServicio);
+router.get('/fechas', pagosAdmController.getPagosPorRangoFechas);
+router.get('/', pagosAdmController.getAdmPagos); // Esta al final
 
 module.exports = router;
